e637741b1c3790d30ad84fc9808c94cd22e01804
Milos Peric <steamestt@gmail.com>
2020-08-25 13:56:17 +0200

    +Add Html downloader

---
+++ Dan_LVI_Milos_Peric/Dan_LVI_Milos_Peric/View/HTTPDownloaderView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Dan_LVI_Milos_Peric.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dan_LVI_Milos_Peric.View
+{
+    /// <summary>
+    /// Interaction logic for HTTPDownloaderView.xaml
+    /// </summary>
+    public partial class HTTPDownloaderView : Window
+    {
+        public HTTPDownloaderView()
+        {
+            InitializeComponent();
+            DataContext = new HTTPDownloaderViewModel(this);
+        }
+    }
+}
---
+++ Dan_LVI_Milos_Peric/Dan_LVI_Milos_Peric/ViewModel/HTTPDownloaderViewModel.cs
@@ -0,0 +1,228 @@
+﻿using Dan_LVI_Milos_Peric.Command;
+using Dan_LVI_Milos_Peric.View;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Dan_LVI_Milos_Peric.ViewModel
+{
+    class HTTPDownloaderViewModel : ViewModelBase
+    {
+        private HTTPDownloaderView hTTPDownloaderView;
+
+        public HTTPDownloaderViewModel(HTTPDownloaderView hTTPDownloaderView)
+        {
+            this.hTTPDownloaderView = hTTPDownloaderView;
+        }
+
+        private string htmlLink;
+        public string HtmlLink
+        {
+            get { return htmlLink; }
+            set
+            {
+                htmlLink = value;
+                OnPropertyChanged("HtmlLink");
+            }
+        }
+
+        private string folderName;
+        public string FolderName
+        {
+            get { return folderName; }
+            set
+            {
+                folderName = value;
+                OnPropertyChanged("FolderName");
+            }
+        }
+
+        private string fileName;
+        public string FileName
+        {
+            get { return fileName; }
+            set
+            {
+                fileName = value;
+                OnPropertyChanged("FileName");
+            }
+        }
+
+        private string htmlFilePath;
+        public string HtmlFilePath
+        {
+            get { return htmlFilePath; }
+            set
+            {
+                htmlFilePath = value;
+                OnPropertyChanged("HtmlFilePath");
+            }
+        }
+
+        private string archiveFileName;
+        public string ArchiveFileName
+        {
+            get { return archiveFileName; }
+            set
+            {
+                archiveFileName = value;
+                OnPropertyChanged("ArchiveFileName");
+            }
+        }
+
+        private string htmlDownload;
+        public string HtmlDownload
+        {
+            get { return htmlDownload; }
+            set
+            {
+                htmlDownload = value;
+                OnPropertyChanged("HtmlDownload");
+            }
+        }
+
+        private ICommand downloadCommand;
+        public ICommand DownloadCommand
+        {
+            get
+            {
+                if (downloadCommand == null)
+                {
+                    downloadCommand = new RelayCommand(param => DownloadCommandExecute(), param => CanDownloadCommandExecute());
+                }
+                return downloadCommand;
+            }
+        }
+
+        private void DownloadCommandExecute()
+        {
+            StringBuilder file = new StringBuilder();
+            file.Append(@"..\..\.\");
+            file.Append(FolderName);
+            file.Append("\\");
+            file.Append(FileName);
+            file.Append(".html");
+            HtmlFilePath = file.ToString();
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    try
+                    {
+                        if (!Directory.Exists(@"..\..\.\" + FolderName))
+                        {
+                            Directory.CreateDirectory(@"..\..\.\" + FolderName);
+                        }
+                        client.DownloadFile(HtmlLink, HtmlFilePath);
+                        MessageBox.Show("File downloaded successfully in Project root directory", "Success!");
+                    }
+                    catch (WebException e)
+                    {
+                        MessageBox.Show("Html link is not valid!", "Warning!");
+                        return;
+                        throw e;                       
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanDownloadCommandExecute()
+        {
+            if (string.IsNullOrEmpty(HtmlLink) || string.IsNullOrEmpty(FolderName) || string.IsNullOrEmpty(FileName))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private ICommand compressCommand;
+        public ICommand CompressCommand
+        {
+            get
+            {
+                if (compressCommand == null)
+                {
+                    compressCommand = new RelayCommand(param => CompressCommandExecute(), param => CanCompressCommandExecute());
+                }
+                return compressCommand;
+            }
+        }
+
+        private void CompressCommandExecute()
+        {
+            try
+            {
+                string dirname = @"..\..\.\" + FolderName;
+                string zippath = @"..\..\.\" + ArchiveFileName + ".zip";
+                if (File.Exists(zippath))
+                {
+                    MessageBox.Show("Archive with same name already exists please choose another name", "Warning!");
+                    return;
+                }
+                if (!File.Exists(HtmlFilePath))
+                {
+                    MessageBox.Show("Noting to compress, please download the web page first.", "Warning!");
+                    return;
+                }
+                ZipFile.CreateFromDirectory(dirname, zippath);
+                MessageBox.Show("File archived successfully in Project root directory");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanCompressCommandExecute()
+        {
+            if (!File.Exists(HtmlFilePath))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private ICommand quitCommand;
+        public ICommand QuitCommand
+        {
+            get
+            {
+                if (quitCommand == null)
+                {
+                    quitCommand = new RelayCommand(param => QuitCommandExecute());
+                }
+                return quitCommand;
+            }
+        }
+
+        private void QuitCommandExecute()
+        {
+            try
+            {
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
